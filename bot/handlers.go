b65bb2bc1bd4a0054f0b52d7bfe75d67f5d1bc32
dgolov <dmitriy.evgenyevich@gmail.com>
2025-01-01 21:42:40 +0300

    upd handlers

--- bot/handlers.go
+++ bot/handlers.go
@@ -12,7 +12,6 @@ import (
 func (b *Bot) HandleUpdates(storage *db.Database)  {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-
 	updates := b.API.GetUpdatesChan(u)
 
 	for update := range updates {
@@ -22,41 +21,17 @@ func (b *Bot) HandleUpdates(storage *db.Database)  {
 
 			switch update.Message.Command() {
 			case "start":
-				msg := tgbotapi.NewMessage(chatID, "Добро пожаловать! Я помогу вам вести учет расходов.")
-				b.API.Send(msg)
-
+				start(b, chatID)
 			case "add":
-				b.SetAwaitingExpense(chatID)
-				msg := tgbotapi.NewMessage(chatID,
-					"Введите расход в формате: <сумма> <категория>.\nЕсли передумали, отправьте /cancel.")
-				b.API.Send(msg)
-
+				add(b, chatID)
 			case "list":
-				expenses, err := storage.ListExpenses(chatID)
-				if err != nil {
-					log.Printf("Get expenses error: %v", err)
-					b.API.Send(tgbotapi.NewMessage(chatID, "Ошибка при получении расходов."))
-					return
-				}
-				if len(expenses) == 0 {
-					b.API.Send(tgbotapi.NewMessage(chatID, "У вас пока нет расходов."))
-				} else {
-					msg := "Ваши расходы:\n" + strings.Join(expenses, "\n")
-					b.API.Send(tgbotapi.NewMessage(chatID, msg))
-				}
-
+				list(b, chatID, storage)
 			case "cancel":
-				if b.AwaitingExpenses[chatID] {
-					b.ResetAwaitingExpense(chatID)
-					b.AwaitingExpenses[chatID] = false
-					msg := tgbotapi.NewMessage(chatID, "Добавление расхода отменено.")
-					b.API.Send(msg)
-				} else {
-					msg := tgbotapi.NewMessage(chatID, "Вы не находитесь в процессе добавления расхода.")
-					b.API.Send(msg)
-				}
-
+				cancel(b, chatID)
 			default:
+				if checkMessage(update.Message.Text, b, chatID, storage) == 1 {
+					continue
+				}
 				if b.AwaitingExpenses[chatID] {
 					if strings.Contains(update.Message.Text, " ") {
 						parts := strings.SplitN(update.Message.Text, " ", 2)
@@ -89,4 +64,90 @@ func (b *Bot) HandleUpdates(storage *db.Database)  {
 			}
 		}
 	}
+}
+
+func checkMessage(text string, b *Bot, chatID int64, storage *db.Database) int8 {
+	switch text {
+	case "Добавить":
+		add(b, chatID)
+		return 1
+	case "Список":
+		list(b, chatID, storage)
+		return 1
+	case "Отмена":
+		cancel(b, chatID)
+		return 1
+	}
+	return 0
+}
+
+func start(b *Bot, chatID int64) {
+	keyboard := getMainKb()
+	msg := tgbotapi.NewMessage(chatID, "Добро пожаловать! Я помогу вам вести учет расходов.")
+	msg.ReplyMarkup = keyboard
+	b.API.Send(msg)
+}
+
+func add(b *Bot, chatID int64)  {
+	keyboard := getCancelKb()
+	b.SetAwaitingExpense(chatID)
+	msg := tgbotapi.NewMessage(chatID,
+		"Введите расход в формате: <сумма> <категория>.\nЕсли передумали, отправьте /cancel.")
+	msg.ReplyMarkup = keyboard
+	b.API.Send(msg)
+}
+
+func list(b *Bot, chatID int64, storage *db.Database) {
+	keyboard := getMainKb()
+	expenses, err := storage.ListExpenses(chatID)
+	if err != nil {
+		log.Printf("Get expenses error: %v", err)
+		msg := tgbotapi.NewMessage(chatID, "Ошибка при получении расходов.")
+		msg.ReplyMarkup = keyboard
+		b.API.Send(msg)
+		return
+	}
+	if len(expenses) == 0 {
+		msg := tgbotapi.NewMessage(chatID, "У вас пока нет расходов.")
+		msg.ReplyMarkup = keyboard
+		b.API.Send(msg)
+	} else {
+		msg := tgbotapi.NewMessage(chatID, "Ваши расходы:\n" + strings.Join(expenses, "\n"))
+		msg.ReplyMarkup = keyboard
+		b.API.Send(msg)
+	}
+}
+
+func cancel(b *Bot, chatID int64) {
+	keyboard := getMainKb()
+	if b.AwaitingExpenses[chatID] {
+		b.ResetAwaitingExpense(chatID)
+		b.AwaitingExpenses[chatID] = false
+		msg := tgbotapi.NewMessage(chatID, "Добавление расхода отменено.")
+		msg.ReplyMarkup = keyboard
+		b.API.Send(msg)
+	} else {
+		msg := tgbotapi.NewMessage(chatID, "Вы не находитесь в процессе добавления расхода.")
+		msg.ReplyMarkup = keyboard
+		b.API.Send(msg)
+	}
+}
+
+func getMainKb() tgbotapi.ReplyKeyboardMarkup {
+	keyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Добавить"),
+			tgbotapi.NewKeyboardButton("Список"),
+		),
+	)
+	return keyboard
+}
+
+func getCancelKb() tgbotapi.ReplyKeyboardMarkup {
+	keyboard := tgbotapi.NewReplyKeyboard(
+		tgbotapi.NewKeyboardButtonRow(
+			tgbotapi.NewKeyboardButton("Отмена"),
+		),
+	)
+	return keyboard
 }
